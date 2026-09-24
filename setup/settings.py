e3d99f5e6428f399a5f8e9112b7b413df6ae1d00
aalphablackk <aalphablackk@gmail.com>
2026-09-24 07:43:17 -0500

    csrf

--- setup/settings.py
+++ setup/settings.py
@@ -66,7 +66,7 @@ ALLOWED_HOSTS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "https://booknest-xxx.vercel.app",
+    "https://bookfest-blond.vercel.app/",
 ]
 
 
